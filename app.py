de81adfb175a27853c9715a82ba1f26fc4e15ed7
diduk001 <didurenkos@gmail.com>
2020-04-14 19:50:40 +0500

    Added password hashing in users_login_data.py Reformatted base.html Added invalid login password alert in login.html Added user auth in app.py Added __str__ method in User() class and id field

--- app.py
+++ app.py
@@ -1,15 +1,29 @@
-from flask import Flask, render_template, current_app, redirect, request
 import json
 
-from config import config
+from flask import Flask, render_template, current_app, redirect, request
+from flask_login import LoginManager, logout_user, login_required, login_user, current_user
+
+import config
 from db_interface import *
 from forms import SignUpForm, LoginForm
 
+# Создание приложения
+
 app = Flask(__name__)
+
+# Создание секретного ключа
+
 app.config['SECRET_KEY'] = "0d645377e31ab6b5847ec817bac4aaf8"
-    
+
+# Создание и инициализация менеджера входа
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+# Конфигурация приложения (см.файл config.py)
+
 with app.app_context():
-    config(current_app)
+    config.config(current_app)
 
 number_of_task = 5
 with open("tasks_info.json", "rt", encoding="utf8") as f:
@@ -18,7 +32,10 @@ for key in tasks.keys():
     print(tasks[key])
     tasks[key]['number'] = number_of_task
 
-    # Вызов обработчика базового шаблона base.html
+
+# Вызов обработчика базового шаблона base.html
+# Это только для дебага, удалите в релизе
+
 @app.route("/base")
 @app.route("/base.html")
 def base():
@@ -29,6 +46,7 @@ def base():
 
 
 # Главная страница
+
 @app.route("/")
 def index():
     params = dict()
@@ -45,20 +63,17 @@ def profile():
     # 1 - открывает "Вход"
     # 2 - открывает "Профиль"
 
-    debug_var = 1
+    debug_var = 0
     if debug_var == 0:
-        return "Это версия для релиза, но я её пока что не сделал"
+        try:
+            if is_auth():
+                return "ползователь не авториозван"
+            if not current_user.is_authenticated:
+                return "user is not authenticated"
+        except AttributeError:
+            return str(get_user_from_id(current_user.id))
     elif debug_var == 1:
-        form = LoginForm()
-        params = dict()
-        params["title"] = "Вход"
-        params["form"] = form
-
-        if form.validate_on_submit():
-            return redirect("/base.html")
-        return render_template("login.html", **params)
-    elif debug_var == 2:
-        return "Это профиль, но я его пока не сделал"
+        return redirect("/login")
 
 
 # Регистрация
@@ -70,11 +85,17 @@ def sign_up():
     params["title"] = "Регистрация"
     params["form"] = sign_up_form
 
+    # Проверяем, правильно ли заполнена форма
+
     if sign_up_form.validate_on_submit():
+
+        # Проверяем, не использован ли уже логин
+
         if check_login(request.form.get("team-login")):
             return render_template("sign_up.html", **params, used_login=True)
 
-        print(repr(request.form.get("team-grade")))
+        # Создания объека User
+
         user = User(
             login=request.form.get("team-login"),
             team_name=request.form.get("team-team_name"),
@@ -98,34 +119,49 @@ def sign_up():
         )
 
         user.set_password(request.form.get("team-password"))
+
+        # Добавление пользователя в базу данных
+
         add_user(user)
-        return redirect("/login")
+        login_user(user, remember=True)
+        return redirect("/")
 
     return render_template("sign_up.html", **params)
 
+
+# Страница с правилами
 
 @app.route('/rules')
 def rules():
     params = dict()
     params["title"] = "Правила"
     return render_template("rules.html", **params)
-  
+
+
 keys = list(map(str, range(1, 29)))
-tasks_names = {'0-0':'1', '0-1':'2', '0-2':'3', '0-3':'4', '0-4':'5', '0-5':'6', '0-6':'7', '1-1':'8', '1-2':'9',
-               '1-3':'10', '1-4':'11', '1-5':'12', '1-6':'13', '2-2':'14', '2-3':'15', '2-4':'16', '2-5':'17',
-               '2-6':'18', '3-3':'19', '3-4':'20', '3-5':'21', '3-6':'22', '4-4':'23', '4-5':'24', '4-6':'25',
-               '5-5':'26', '5-6':'27', '6-6':'28'}
+tasks_names = {'0-0': '1', '0-1': '2', '0-2': '3', '0-3': '4', '0-4': '5', '0-5': '6', '0-6': '7',
+               '1-1': '8', '1-2': '9',
+               '1-3': '10', '1-4': '11', '1-5': '12', '1-6': '13', '2-2': '14', '2-3': '15',
+               '2-4': '16', '2-5': '17',
+               '2-6': '18', '3-3': '19', '3-4': '20', '3-5': '21', '3-6': '22', '4-4': '23',
+               '4-5': '24', '4-6': '25',
+               '5-5': '26', '5-6': '27', '6-6': '28'}
 number_of_picked_task = 0
 picked_tasks = []
+
+
+# Страница домино
 
 @app.route("/domino", methods=["GET", "POST", "PATCH"])
 def domino():
     global number_of_picked_task, picked_tasks, keys, tasks
-    messages = {'full': "Вы уже взяли 2 задачи", 'accepted':'Вы уже решили эту задачу',
-                'failed': 'У Вас закончились попытки на сдачу этой задачи', 'absent': 'На данный момент задачи с этим номером отсутсвуют',
+    messages = {'full': "Вы уже взяли 2 задачи", 'accepted': 'Вы уже решили эту задачу',
+                'failed': 'У Вас закончились попытки на сдачу этой задачи',
+                'absent': 'На данный момент задачи с этим номером отсутсвуют',
                 'hand': 'Вы уже взяли эту задачу'}
     if request.method == "GET":
-        return render_template("domino.html", title="Домино ТЮМ72", block="", tasks=tasks, keys=keys, picked_tasks=picked_tasks, message=False)
+        return render_template("domino.html", title="Домино ТЮМ72", block="", tasks=tasks, keys=keys,
+                               picked_tasks=picked_tasks, message=False)
     elif request.method == "POST":
         message = False
         if request.form.get("picked"):
@@ -147,12 +183,77 @@ def domino():
                 tasks[key]['state'] = verdicts[verdicts.index(tasks[key]['state']) + 1]
             picked_tasks.remove(key)
             number_of_picked_task -= 1
-        return render_template("domino.html", title="Домино ТЮМ72", block="", tasks=tasks, keys=keys, picked_tasks=picked_tasks, message=message)
+        return render_template("domino.html", title="Домино ТЮМ72", block="", tasks=tasks, keys=keys,
+                               picked_tasks=picked_tasks, message=message)
 
+
+# Страница пенальти
 
 @app.route('/penalty')
 def penalty():
     return "Я захотел поспать и поэтому не сделал эту страницу"
+
+
+# Страница для авторизации
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    params = dict()
+    params["title"] = "Вход"
+    params["form"] = form
+    if form.validate_on_submit():
+        session = db_session.create_session()
+        user = session.query(UserLoginData).filter(UserLoginData.login == form.login.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=True)
+            return redirect("/")
+        return render_template('login.html',
+                               invalid_parameters=True,
+                               form=form)
+    return render_template("login.html", **params)
+
+
+# Выход из аккаунта
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+# Функция загрузки данных пользователя из базы данных
+# user_id - id команды
+# login_data - bool, если True, то загружает данные авторизации, иначе данные о членах команды
+def load_user(user_id, login_data):
+    if login_data:
+        load_user_login_data(user_id)
+    else:
+        load_user_members_data(user_id)
+
+
+# Функция для получения данных авторизации пользователя по id
+@login_manager.user_loader
+def load_user_login_data(user_id):
+    session = db_session.create_session()
+    return session.query(UserLoginData).get(user_id)
+
+
+# Функция для получения данных о команде по id
+@login_manager.user_loader
+def load_user_members_data(user_id):
+    session = db_session.create_session()
+    return session.query(UserMemberData).get(user_id)
+
+
+# Функция возвращает True если пользователь авторизован, иначе False
+def is_auth():
+    try:
+        if not current_user.is_authenticated:
+            return False
+    except AttributeError:
+        return True
 
 
 if __name__ == '__main__':
--- data/users_login_data.py
+++ data/users_login_data.py
@@ -1,10 +1,12 @@
 import sqlalchemy
+from flask_login import UserMixin
 from sqlalchemy import orm
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from .db_session import SqlAlchemyBase
 
 
-class UserLoginData(SqlAlchemyBase):
+class UserLoginData(SqlAlchemyBase, UserMixin):
     __tablename__ = 'users_login_data'
 
     id = sqlalchemy.Column(sqlalchemy.Integer,
@@ -12,3 +14,9 @@ class UserLoginData(SqlAlchemyBase):
     login = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     user_id = orm.relation("UserMembersData", back_populates='user')
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
